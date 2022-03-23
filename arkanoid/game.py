eec9cb66325720a06900633500c5fac3bf1f6fea
Sergey <sproshchenko@yandex.ru>
2022-03-23 15:53:01 +0300

    - added collision detection to Arkanoid game

--- arkanoid/game.py
+++ arkanoid/game.py
@@ -12,10 +12,11 @@ class Direction(Enum):
     LEFT = 2
     UP = 3
     DOWN = 4
-    LUNCH = 5
+    SPACE = 5
 
 
 Point = namedtuple('Point', 'x0 x1 y0 y1')
+PointBall = namedtuple('PointBall', 'x0 y0 r')
 
 WHITE = (255, 255, 255)
 RED = (200, 0, 0)
@@ -29,7 +30,7 @@ BLACK = (0, 0, 0)
 BLOCK_SIZE = 20
 SPACE_BETWEEN_BLOCKS = 2
 GOAL_BLOCK_SIZE = BLOCK_SIZE * 2
-SPEED = 20
+SPEED = 40
 
 
 class ArkanoidGame:
@@ -54,12 +55,17 @@ class ArkanoidGame:
     def reset(self):
         self.direction = None
         self.racket = Point((self.w / 2) - self.racket_size / 2, (self.w / 2) + self.racket_size / 2,
-              self.h - 30, self.h - 30 - BLOCK_SIZE)
-        self.ball = Point((self.w / 2) - BLOCK_SIZE / 2, (self.w / 2) + BLOCK_SIZE / 2,
-              self.h - 30 - BLOCK_SIZE, self.h - 30 - BLOCK_SIZE * 2)
+              self.h - 30 - BLOCK_SIZE, self.h - 30)
+
+        self.ball_motion = False
+        self.ball_speed_x = 0
+        self.ball_speed_y = 0
+        self.ball = PointBall((self.w / 2), self.racket.y0 - BLOCK_SIZE / 2, BLOCK_SIZE / 2)
+
         self.blocks.clear()
-        self.score = 0
         self._place_blocks()
+
+        self.score = 0
         self.frame_iteration = 0
 
     def get_edge_space_size(self):
@@ -94,13 +100,16 @@ class ArkanoidGame:
                     self.direction = Direction.LEFT
                 elif event.key == pygame.K_RIGHT:
                     self.direction = Direction.RIGHT
+                elif event.key == pygame.K_SPACE:
+                    self.direction = Direction.SPACE
 
         self._move(self.direction)
+        game_over = self._ball_move(self.direction)
         self._update_ui()
 
         self.direction = None
         self.clock.tick(SPEED)
-        return False, self.score
+        return game_over, self.score
 
     def _move(self, direction):
         difference = 0
@@ -109,13 +118,129 @@ class ArkanoidGame:
         elif direction == Direction.LEFT:
             difference -= BLOCK_SIZE
 
-        if self.is_edge(difference) is True:
+        if self.is_racket_edge(difference) is True:
             self.racket = Point(self.racket.x0 + difference, self.racket.x1 + difference,
                                 self.racket.y0,  self.racket.y1)
 
-    def is_edge(self, difference):
+    def is_racket_edge(self, difference):
         x0, x1, y0, y1 = self.racket
         return True if self.w >= x1 + difference and x0 + difference >= 0 else False
+
+    def _ball_move(self, direction):
+        if direction == Direction.SPACE and self.ball_motion is False:
+            self.ball_motion = True
+            self.ball_speed_x = random.randint(-4, 4)
+            self.ball_speed_y = random.randint(-3, -3)
+
+        game_over = False
+        if self.ball_motion is True:
+            next_position = self.get_next_position()
+            next_position, game_over = self.is_ball_edge(next_position)
+            next_position = self.is_other_subject(next_position)
+            self.ball = next_position
+        return game_over
+
+    def get_next_position(self):
+        return PointBall(self.ball.x0 + self.ball_speed_x, self.ball.y0 + self.ball_speed_y, self.ball.r)
+
+    def is_ball_edge(self, next_pos):
+        gg = False
+
+        if next_pos.x0 - next_pos.r <= 0:
+            next_pos = PointBall(next_pos.r + abs(next_pos.x0 - next_pos.r), abs(next_pos.y0), next_pos.r)
+            self.ball_speed_x *= -1
+
+        elif next_pos.x0 + next_pos.r >= self.w:
+            next_pos = PointBall(self.w - abs(next_pos.x0 + next_pos.r - self.w) - next_pos.r,
+                             next_pos.y0, next_pos.r)
+            self.ball_speed_x *= -1
+
+        if next_pos.y0 - next_pos.r <= 0:
+            next_pos = PointBall(abs(next_pos.x0), next_pos.r + abs(next_pos.y0 - next_pos.r), next_pos.r)
+            self.ball_speed_y *= -1
+        elif next_pos.y0 + next_pos.r >= self.h:
+            gg = False
+            next_pos = PointBall(next_pos.x0,
+                                 self.h - abs(next_pos.y0 + next_pos.r - self.h) - next_pos.r, next_pos.r)
+            self.ball_speed_y *= -1
+
+        return next_pos, gg
+
+    def is_other_subject(self, next_step):
+        xDist, yDist = self.get_distance(next_step, self.racket)
+        if self.is_collision(xDist, yDist, next_step) is True and self.ball_motion is True:
+            side = self.get_side(xDist, yDist)
+            k = self.get_coefficient(next_step)
+            self.change_speed(side, is_racket=True, coefficient=k)
+
+        for i, item in enumerate(self.blocks):
+            xDist, yDist = self.get_distance(next_step, item)
+            if self.is_collision(xDist, yDist, next_step) is True:
+                side = self.get_side(xDist, yDist)
+                self.blocks.pop(i)
+                self.change_speed(side)
+                break
+
+        return next_step
+
+    def get_distance(self, ball, brick):
+        brick_width = brick.x1 - brick.x0
+        brick_height = brick.y1 - brick.y0
+        xDist = self.calculate_distance(ball.x0, brick.x1 - brick_width / 2) - brick_width / 2
+        yDist = self.calculate_distance(ball.y0, brick.y1 - brick_height / 2) - brick_height / 2
+        return xDist, yDist
+
+    def is_collision(self, xDist, yDist, ball):
+        return True if xDist < ball.r and yDist < ball.r else False
+
+    @staticmethod
+    def calculate_distance(a, b):
+        return abs(a-b)
+
+    def get_side(self, xDist, yDist):
+        if self.ball_speed_x < 0 and self.ball_speed_y < 0:
+            if xDist < yDist:
+                return "BOTTOM"
+            elif xDist == yDist:
+                return "CORNER"
+            else:
+                return "RIGHT"
+        elif self.ball_speed_x < 0 and self.ball_speed_y >= 0:
+            if xDist < yDist:
+                return "TOP"
+            elif xDist == yDist:
+                return "CORNER"
+            else:
+                return "RIGHT"
+        elif self.ball_speed_x >= 0 and self.ball_speed_y < 0:
+            if xDist < yDist:
+                return "BOTTOM"
+            elif xDist == yDist:
+                return "CORNER"
+            else:
+                return "LEFT"
+        elif self.ball_speed_x >= 0 and self.ball_speed_y >= 0:
+            if xDist < yDist:
+                return "TOP"
+            elif xDist == yDist:
+                return "CORNER"
+            else:
+                return "LEFT"
+
+    def get_coefficient(self, ball):
+        return (ball.x0 - self.racket.x0) / (self.racket.x1 - self.racket.x0)
+
+    def change_speed(self, side, is_racket=False, coefficient=None):
+        if is_racket is False:
+            if side == "RIGHT" or side == "LEFT":
+                self.ball_speed_x *= (-1)
+            else:
+                self.ball_speed_y *= (-1)
+        if is_racket is True:
+            self.ball_speed_y *= (-1)
+            if self.ball_speed_x > 0 and coefficient < 0.5 or self.ball_speed_x < 0 and coefficient >= 0.5:
+                p = abs(1 - coefficient) / 1
+                self.ball_speed_x *= (-1)
 
     def _update_ui(self):
         self.display.fill(WHITE)
@@ -123,14 +248,12 @@ class ArkanoidGame:
         border_size = 2
         pygame.draw.rect(self.display, DARK_BLUE, pygame.Rect(self.racket.x0, self.racket.y0,
                                                          self.racket.x1 - self.racket.x0,
-                                                         self.racket.y0 - self.racket.y1))
+                                                         self.racket.y1 - self.racket.y0))
         pygame.draw.rect(self.display, BLUE, pygame.Rect(self.racket.x0 + border_size, self.racket.y0 + border_size,
                                                          self.racket.x1 - self.racket.x0 - border_size * 2,
-                                                         self.racket.y0 - self.racket.y1 - border_size * 2))
+                                                         self.racket.y1 - self.racket.y0 - border_size * 2))
 
-        pygame.draw.rect(self.display, DARK_RED, pygame.Rect(self.ball.x0, self.ball.y0, BLOCK_SIZE, BLOCK_SIZE), border_radius=8)
-        pygame.draw.rect(self.display, RED, pygame.Rect(self.ball.x0 + 1, self.ball.y0 + 1,
-                                                        BLOCK_SIZE - 1 * 2, BLOCK_SIZE - 1 * 2), border_radius=8)
+        pygame.draw.circle(self.display, DARK_RED, (round(self.ball.x0), round(self.ball.y0)), self.ball.r)
 
         for item in self.blocks:
             pygame.draw.rect(self.display, DARK_GREEN, pygame.Rect(item.x0, item.y0, GOAL_BLOCK_SIZE, BLOCK_SIZE))
